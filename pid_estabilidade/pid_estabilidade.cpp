b5f6b99d8c33ee8d0cfeda5166379f5ad5c4478b
mmazzarino <matheus.mazzarino@acad.pucrs.br>
2019-03-27 21:04:00 -0300

    Continuação binlioteca

--- pid_estabilidade/pid_estabilidade.cpp
+++ pid_estabilidade/pid_estabilidade.cpp
@@ -6,10 +6,26 @@ pid_estabilidade::pid_estabilidade(int pin_motor_a, int pin_motor_b)
 	motor_b = pin_motor_b;
 }
 
-int pid_estabilidade::controle_acl(float acl, float sp_acl, int pwm_ma, int pwm_mb, unsigned long dt){
+void pid_estabilidade::controle_acl(float acl, float sp_acl, int pwm_ma, int pwm_mb, unsigned long dt){
+	float p = 0, i = 0, d = 0, erro = 0, pid = 0;
+	int retorno;
 	
+	erro = sp_acl - acl;
+	p = cp_acl * erro;
+	i += (ci_acl * erro) * dt;
+	d = (erro * cd_acl) / dt;
+	pid = p + i + d;
+	
+	if(pid >= 0){
+		digitalWrite(motor_a, high);
+		digitalWrite(motor_b, high);
+	}
+	else{
+		digitalWrite(motor_a, low);
+		digitalWrite(motor_b, low);
+	}
 }
 
-int pid_estabilidade::controle_gir(float gir, float sp_gir, int pwm, int pwm_ma, int pwm_mb, unsigned long dt){
+void pid_estabilidade::controle_gir(float gir, float sp_gir, int pwm, int pwm_ma, int pwm_mb, unsigned long dt){
 	
 }
--- pid_estabilidade/pid_estabilidade.h
+++ pid_estabilidade/pid_estabilidade.h
@@ -1,4 +1,12 @@
 #include <Arduino.h>
+#define cp_acl                //constantes 
+#define cp_gir
+#define ci_acl
+#define ci_gir
+#define cd_acl
+#define cd_gir 
+
+
 
 class pid_estabilidade
 { 
@@ -6,9 +14,9 @@ class pid_estabilidade
 public:
     pid_estabilidade(int pin_motor_a, int pin_motor_b); //construtor
     
-    int controle_acl(float acl, float sp_acl, int pwm_ma, int pwm_mb, unsigned long dt);
+    void controle_acl(float acl, float sp_acl, int pwm_ma, int pwm_mb, unsigned long dt);
     
-    int controle_gir(float gir, float sp_gir, int pwm_ma, int pwm_mb, unsigned long dt);
+    void controle_gir(float gir, float sp_gir, int pwm_ma, int pwm_mb, unsigned long dt);
   
 private:
     float set_point;
